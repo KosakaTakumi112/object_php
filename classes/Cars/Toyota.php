85c5cb16cfe909ee1534641e6c3ff2b5009fbc35
Your Name <you@example.com>
2022-04-18 16:20:10 +0900

    add toyota

---
+++ classes/Cars/Toyota.php
@@ -0,0 +1,34 @@
+<?php
+
+  require_once "car.php";
+
+  class Toyota extends Car{
+
+    function __construct(
+      $name = "トヨタ",
+      $price_jpy         = 20000000,
+      $seating_capacity  = 4,
+      $seating_number    = 1,
+      $velocity_kmph     = 0,
+      $max_velocity_kmph = 200,
+      $acceleration_mpss = 9,
+      $deceleration_mpss = -12,
+      $height_cm = 130,
+    ){
+      parent::__construct(
+        $name,
+        $price_jpy,
+        $seating_capacity,
+        $seating_number,
+        $velocity_kmph,
+        $max_velocity_kmph,
+        $acceleration_mpss = round($prices/45,2),
+        $deceleration_mpss,
+        $height_cm
+      );
+
+    }
+
+  }
+
+?>
